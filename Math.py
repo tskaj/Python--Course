78798e19a19d77d90d10faf088d35c329da76714
tskaj <majaved770@gmail.com>
2024-07-15 01:35:15 -0700

    more changes to the file

--- Math.py
+++ Math.py
@@ -1,5 +1,7 @@
 import math
 from math import factorial as fac
+from math import sqrt as root
+from math import pow as power
 
 #printing factorial using dot operator for math library
 print(math.factorial(5))
@@ -16,3 +18,8 @@ print ("You can draw",ways,"ways 2 apples from 100 apples")
 print (fac(n)) #This gives us a big number
 
 print(len(str(fac(n)))) #This will give us the number of digits in factorial n
+
+for i in range (5): #print the square root from 1 to 5 using for loop
+    print (int(root(i))) #typecasting the float values to integer values
+
+print (power(2,5)) #print 2 raised to the power 5
